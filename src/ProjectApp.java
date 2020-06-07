22c7d37b9e342f1e0e641c75e28aae7a2dd25726
Mike <lammara96@gmail.com>
2020-06-07 03:59:19 -0400

    finished project, just needs comments

--- src/ProjectApp.java
+++ src/ProjectApp.java
@@ -1,69 +1,159 @@
-import entities.*;
-import entities.enemies.Enemy;
+import entities.Entity;
 import entities.enemies.Necromancer;
 import entities.enemies.Rogue;
 import entities.npc.Mage;
-import entities.npc.NPC;
 import entities.npc.Trainer;
 import util.UserInputHelper;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
 
+/**
+ * The Class to run the project application
+ */
 public class ProjectApp {
-
-    private final String[] ENTITY_NAME_LIST = {"Mage", "Necromancer", "Rogue", "Trainer"};
-
-    private ArrayList<Entity> entities;
+    private static final List<String> ENTITY_NAME_LIST = List.of("Mage", "Necromancer", "Rogue", "Trainer");
+    private final Map<String, Entity> entityMap;
+    
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+    
+    /**
+     * init map and call first menu display
+     */
     public ProjectApp() {
-
-        entities = new ArrayList<>();
+        entityMap = new LinkedHashMap<>();
+        
+        /* For testing purposes
+        
+        Mage mage = new Mage("magey", 1, 70, "frostbolt", 1, "frost", 50);
+        Trainer trainer = new Trainer("trainerguy", 1, 10, "spell", 1, "nothing");
+        Trainer trainer1 = new Trainer("idiot", 1, 150, "spelll", 2, "nothing");
+        entityMap.put(mage.getName(), mage);
+        entityMap.put(trainer.getName(), trainer);
+        entityMap.put(trainer1.getName(), trainer1);
+        */
+        
         displayMenu();
     }
-
-
+    
+    
     /**
      * Display the list of menu options for performing actions on the list of entities
      */
     public void displayMenu() {
-
+        
         System.out.println("____________________________________________________"
-                        + "\n1. View Entities"
-                        + "\n2. Add Entity"
-                        + "\n3. Remove Entity"
-                        + "\n4. Perform an Action"
-                        + "\n5. End Program"
-                        + "\nEnter a number 1-5 to perform an action");
-
-
-        switch (getUserMenuInput(5)) {
-            /*case -1: displayMenu();
-            break;*/
-            case 1 : printEntities();
-            break;
-            case 2 : addEntityMenu();
-            break;
-            case 3 : removeEntity();
-            break;
-            case 4 : performAction();
-            break;
-            case 5 :
+                + "\n1. View Entities"
+                + "\n2. Add Entity"
+                + "\n3. Remove Entity"
+                + "\n4. Perform an Action"
+                + "\n5. End Program"
+                + "\nEnter a number 1-5 to perform an action");
+        
+        
+        switch (UserInputHelper.getMenuInput(5)) {
+            case 1:
+                printEntities();
+                break;
+            case 2:
+                addEntityMenu();
+                break;
+            case 3:
+                removeEntity();
+                break;
+            case 4:
+                performAction();
+                break;
+            case 5:
                 System.out.println("Ending Program");
-            break;
+                break;
             default:
                 System.out.println("Invalid input please try again\n");
                 displayMenu();
-            break;
+                break;
         }
     }
-
+    
     public void performAction() {
-        //Todo
-
+        Entity e;
+        System.out.println("Enter the name of the entity you would like to perform an action with: ");
+        String input = UserInputHelper.getStringInput();
+        e = findEntityInList(input);
+        
+        if (e != null) {
+            System.out.println("Entity " + e.getName() + " found in list\n");
+            boolean canFly = (e instanceof Mage || e instanceof Necromancer);
+            
+            System.out.println("Choose an action to perform (Enter a number): "
+                                + "\n1. Speak"
+                                + "\n2. Ready Up"
+                                + "\n3. Move"
+                                + (canFly ? "\n4. Fly" + "\n5. Teleport" : ""));
+            
+            int action = UserInputHelper.INVALID_INPUT;
+            while (action == UserInputHelper.INVALID_INPUT)
+                    action = UserInputHelper.getMenuInput(canFly ? 3 : 5);
+            
+            
+            //not having these methods in the base class makes this so much messier
+            switch (action) {
+                case 1 :
+                    if (e instanceof Mage)
+                        ((Mage) e).speak();
+                    else if (e instanceof Necromancer)
+                        ((Necromancer) e).speak();
+                    else if (e instanceof Trainer)
+                        ((Trainer) e).speak();
+                    else
+                        ((Rogue) e).speak();
+                    break;
+                    
+                case 2 :
+                    if (e instanceof Mage)
+                        ((Mage) e).ready();
+                    else if (e instanceof Necromancer)
+                        ((Necromancer) e).ready();
+                    else if (e instanceof Trainer)
+                        ((Trainer) e).ready();
+                    else
+                        ((Rogue) e).ready();
+                    break;
+                    
+                case 3 :
+                    if (e instanceof Mage)
+                        ((Mage) e).move();
+                    else if (e instanceof Necromancer)
+                        ((Necromancer) e).move();
+                    else if (e instanceof Trainer)
+                        ((Trainer) e).move();
+                    else
+                        ((Rogue) e).move();
+                    break;
+                    
+                case 4 :
+                    if (e instanceof Mage)
+                        ((Mage) e).fly();
+                    else if (e instanceof Necromancer)
+                        ((Necromancer) e).fly();
+                    break;
+    
+                case 5 :
+                    if (e instanceof Mage)
+                        ((Mage) e).teleport();
+                    else if (e instanceof Necromancer)
+                        ((Necromancer) e).teleport();
+                    break;
+            }
+            displayMenu();
+        }
+        System.out.println("Entity " + input + " not found in the list. Would you like to retype the name? (Enter y/n)");
+    
+        if (UserInputHelper.getBooleanInput())
+            performAction();
+        else
+            displayMenu();
     }
-
+    
     /**
      * remove an entity from the list
      * get user input for name of entity, compare to the list
@@ -72,236 +162,103 @@ public class ProjectApp {
     public void removeEntity() {
         String name;
         System.out.println("Enter entity's name for deletion: ");
-        name = getUserInputAnswer();
-
-        if (entities.remove(findEntityInList(name))) {
+        name = UserInputHelper.getStringInput();
+        //System.out.println(entityMap);
+        
+        if (entityMap.remove(name) != null) {
             System.out.println("Entity " + name + " was successfully removed from the list");
             return;
         }
         System.out.println("Entity " + name + " was not found in the list would you like to try entering the name again? (Enter y/n)");
         char c = ' ';
         while (c != 'y' && c != 'n') {
-            c = getUserInputAnswer().charAt(0);
-            if (c < 97)
+            c = UserInputHelper.getStringInput().charAt(0);
+            if (c < 97) {
                 c += 32;
-            System.out.println("**" + c);
-            if (c != 'y' && c != 'n')
+            }
+            //System.out.println("**" + c);
+            if (c != 'y' && c != 'n') {
                 System.out.println("Invalid input enter y/n");
+            }
         }
-        if (c == 'y')
+        if (c == 'y') {
             removeEntity();
-
+        }
+        
         displayMenu();
     }
-
+    
     /**
-     *
      * @param name String to search the list for
      * @return the Entity in the list if it exists
-     *              if it doesn't exist return null
+     * if it doesn't exist return null
      */
     private Entity findEntityInList(String name) {
-        if (entities.isEmpty())
+        if (entityMap.isEmpty()) {
             return null;
-        for (Entity x : entities) {
-            if (x.equals(name))
-                return x;
         }
-        return null;
+        return entityMap.get(name.toLowerCase());
     }
-
+    
     /**
      * display choices of entities for adding to the list
-     *
      */
     public void addEntityMenu() {
-
-
         System.out.println("\nChoose an Entity to add: ");
-        for (int x = 1; x <= ENTITY_NAME_LIST.length; x++) {
-            System.out.println(x + ". " + ENTITY_NAME_LIST[x - 1]);
+        for (int i = 0; i < ENTITY_NAME_LIST.size(); i++) {
+            String name = ENTITY_NAME_LIST.get(i);
+            System.out.println(i + 1 + ". " + name);
         }
-        System.out.println("Enter a number between 1 and " + ENTITY_NAME_LIST.length + " to add to the list");
-
+        
+        System.out.println("Enter a number between 1 and " + ENTITY_NAME_LIST.size() + " to add to the list");
         Entity entityToAdd = null;
-
+        
         //Mage Necromancer Rogue Trainer
-        switch (getUserMenuInput(ENTITY_NAME_LIST.length)) {
-            case -1: addEntityMenu();
-            break;
-            case 1 : entityToAdd = getEntityInformation(Mage.ID);
-            break;
-            case 2 : entityToAdd = getEntityInformation(Necromancer.ID);
-            break;
-            case 3 : entityToAdd = getEntityInformation(Rogue.ID);
-            break;
-            case 4 : entityToAdd = getEntityInformation(Trainer.ID);
-            break;
+        switch (UserInputHelper.getMenuInput(ENTITY_NAME_LIST.size())) {
+            case -1:
+                addEntityMenu();
+                break;
+            case 1:
+                entityToAdd = getEntityInformation(Mage.ID);
+                break;
+            case 2:
+                entityToAdd = getEntityInformation(Necromancer.ID);
+                break;
+            case 3:
+                entityToAdd = getEntityInformation(Rogue.ID);
+                break;
+            case 4:
+                entityToAdd = getEntityInformation(Trainer.ID);
+                break;
             default:
                 System.out.println("Invalid input please try again");
                 addEntityMenu();
         }
-        entities.add(entityToAdd);
-        if (entityToAdd != null)
+        if (entityToAdd != null) {
             System.out.println("\nEntity: " + entityToAdd.getName() + " successfully added to list\n");
+            entityMap.put(entityToAdd.getName().toLowerCase(), entityToAdd);
+        }
         displayMenu();
     }
-
+    
     /**
-     *
      * @param entityID Unique ID of Entity Type
-     *                 0 - Base class (never should be called)
-     *                 IDs beginning with 1 are Enemies
-     *                 IDs beginning with 2 are NPC/Friendlies
      * @return returns an object of a subclass of Entity
-     *          object type depends on entityID
+     * object type depends on entityID
      */
     Entity getEntityInformation(int entityID) {
-
         switch (entityID) {
-            case Rogue.ID :
+            case Rogue.ID:
                 return new Rogue().build();
-            case Necromancer.ID :
+            case Necromancer.ID:
                 return new Necromancer().build();
-            case Mage.ID :
+            case Mage.ID:
                 return new Mage().build();
-            case Trainer.ID :
+            case Trainer.ID:
                 return new Trainer().build();
             default:
                 break;
-
         }
-
-        /*
-        String name;
-        int lives, health;
-
-        System.out.println("Enter Entity name: ");
-        name = getUserInputAnswer();
-
-        for (int x = 0; x < name.length(); x++) {
-            char c = name.charAt(x);
-
-            if ((c < 65 || c > 122) || (c > 90 && c < 97)) {
-                System.out.println("Please only enter letters for entity names" +
-                        "\nPlease start over\n");
-                getEntityInformation(entityID);
-            }
-
-        }
-
-        System.out.println("Enter Entity Lives (integer): ");
-        lives = Integer.parseInt(getUserInputAnswer());
-        System.out.println("Enter Entity Health (integer): ");
-        health = Integer.parseInt(getUserInputAnswer());
-
-
-
-
-
-        would much rather init all these values in the classes constructors
-        this looks very messy
-
-         */
-        /*
-        switch (firstDigit(entityID)) {
-
-            case NPC.ID :
-
-                String helpfulSkill;
-                int skillLevel;
-
-                System.out.println("Enter " + name + "'s helpful skill name: ");
-                helpfulSkill = getUserInputAnswer();
-                System.out.println("Enter " + name + "'s skill level (integer): ");
-                skillLevel = Integer.parseInt(getUserInputAnswer());
-
-            switch (entityID) {
-                case Mage.ID:
-
-                    String damageType;
-                    int yearsPracticing;
-
-                    System.out.println("Enter " + name + "'s damage type:");
-                    damageType = getUserInputAnswer();
-                    System.out.println("Enter " + name + "'s amount of years spent practicing (integer): ");
-                    yearsPracticing = Integer.parseInt(getUserInputAnswer());
-
-                    return new Mage(name, lives, health, helpfulSkill, skillLevel, damageType, yearsPracticing);
-                case Trainer.ID:
-
-                    String expertise;
-
-                    System.out.println("Enter " + name + "'s expertise (area of study): ");
-                    expertise = getUserInputAnswer();
-
-                    return new Trainer(name, lives, health, helpfulSkill, skillLevel, expertise);
-                default:
-                break;
-            }
-            break;
-
-            case Enemy.ID :
-
-                String damageType;
-                int damageLevel;
-
-                System.out.println("Enter " + name + "'s damage type: ");
-                damageType = getUserInputAnswer();
-                System.out.println("Enter " + name + "'s damage level (integer): ");
-                damageLevel = Integer.parseInt(getUserInputAnswer());
-
-            switch (entityID) {
-                case Rogue.ID :
-
-                    int sneakLevel;
-                    boolean master;
-
-                    System.out.println("Enter " + name + "'s sneak level (integer): ");
-                    sneakLevel = Integer.parseInt(getUserInputAnswer());
-                    System.out.println("Is " + name + " a master? (Enter y/n): ");
-                    char c = ' ';
-                    while (c != 'y' && c != 'n') {
-                        c = getUserInputAnswer().charAt(0);
-                        if (c < 97)
-                            c += 32;
-                        if (c != 'y' && c != 'n')
-                            System.out.println("Invalid input enter y/n");
-                    }
-                    master = c == 'y';
-
-                    return new Rogue(name, lives, health, damageType, damageLevel, sneakLevel, master);
-
-                case Necromancer.ID :
-
-                    int numMinions;
-                    boolean elite;
-
-                    System.out.println("Enter " + name + "'s number of minions (integer): ");
-                    numMinions = Integer.parseInt(getUserInputAnswer());
-                    System.out.println("Is " + name + "elite? (Enter y/n): ");
-                    c = ' ';
-                    while (c != 'y' && c != 'n') {
-                        c = getUserInputAnswer().charAt(0);
-                        if (c < 97)
-                            c += 32;
-                        if (c != 'y' && c != 'n')
-                            System.out.println("Invalid input enter y/n");
-                    }
-                    elite = c == 'y';
-
-                    return new Necromancer(name, lives, health, damageType, damageLevel, elite, numMinions);
-
-                default:
-                break;
-
-            }
-            break;
-
-            default:
-            break;
-        }
-        */
         return null;
     }
     
@@ -309,44 +266,23 @@ public class ProjectApp {
      * print the list of entities
      */
     public void printEntities() {
-
-        if (entities.isEmpty()) {
+    
+        if (entityMap.isEmpty()) {
             System.out.println("List is empty");
             displayMenu();
         }
-        for (Entity e : entities) {
+        
+        LinkedHashMap<String, Entity> sortedMap = new LinkedHashMap<>();
+    
+        entityMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+        
+        
+        for (Entity e : sortedMap.values()) {
             System.out.println(e + "\n");
         }
         displayMenu();
-
     }
-
-    /**
-     *
-     * @param max the maximum allowed user input
-     *                  ex. menu options 1-5 then 5 would be max input
-     * @return the user choice, an integer (0, max]
-     *                  return -1 if an invalid option was entered
-     *
-     */
-    private int getUserMenuInput(int max) {
-
-        try {
-
-            String inputString = reader.readLine();
-            if (inputString.charAt(0) < 49 || inputString.charAt(0) > max + 48)
-                return -1;
-
-            return inputString.charAt(0) - 48;
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return -1;
-
-    }
-
-
-    
-
 }
